bd4a04c6b08a3a0e2615a31d73fc9302db8cbe93
mike0z <mikeoz@grdwrx.com>
2023-12-02 12:07:02 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send("Welcome to the home page");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(3000, () => {
+    console.log("Wazzzzapp");
+});
