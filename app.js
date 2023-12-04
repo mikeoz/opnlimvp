2bf94b65a4cc47cb2e3131cc412dd87e11377041
mike0z <mikeoz@grdwrx.com>
2023-12-04 07:47:56 -0600

    third try

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ const express = require('express');
 const app = express();
 
 const PORT = process.env.PORT || 3000;
-app.listen(port, () => {
-    console.log("Wazzzzapp");
-});
 
 app.get('/', (req, res) => {
     res.send("Welcome to the home page");
 });
+
+app.listen(port, () => {
+    console.log("Wazzzzapp");
+});
+
+
