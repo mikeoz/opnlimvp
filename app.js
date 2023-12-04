54f40264eac280da65adba3114f0eb0749bafe42
mike0z <mikeoz@grdwrx.com>
2023-12-04 08:04:37 -0600

    one more time

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
 
-
 app.get('/', (req, res) => {
     res.send("Welcome to the home page");
 });
 
+const port = process.env.port || 3000;
 app.listen(3000,  () => {
     console.log("Wazzzzapp");
 });
