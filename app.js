d52368b746fac3830e30ca829c04d4c2a993d1c6
mike0z <mikeoz@grdwrx.com>
2023-12-04 07:55:04 -0600

    port tweak

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const app = express();
 
-const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
     res.send("Welcome to the home page");
 });
 
-app.listen(port, () => {
+app.listen(3000,  () => {
     console.log("Wazzzzapp");
 });
 
