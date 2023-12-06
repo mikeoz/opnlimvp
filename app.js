d40d48dda0ecc7f1ffca415f766668eae08eb1aa
mike0z <mikeoz@grdwrx.com>
2023-12-06 12:40:47 -0600

    server fix

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ const neo4j = require('neo4j-driver');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Neo4j database connection details
-const NEO4J_URI = 'neo4j://e9ccf68a.databases.neo4j.io';
+// Corrected Neo4j database connection details
+const NEO4J_URI = 'neo4j+s://e9ccf68a.databases.neo4j.io'; // Ensure the use of neo4j+s
 const NEO4J_USER = 'neo4j';
 const NEO4J_PASSWORD = 'Iw-YJYK1DLRlqvjd2BrpfKBTcSUqoPcNGo_mqNEWHQ';
 
@@ -17,8 +17,8 @@ const driver = neo4j.driver(
     { maxTransactionRetryTime: 30000 }
 );
 
-// Middleware to parse URL-encoded bodies
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json()); // Middleware to parse JSON bodies
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
@@ -28,17 +28,27 @@ app.get('/', (req, res) => {
 app.get('/test-db-connection', async (req, res) => {
     const session = driver.session();
     try {
-        const writeTxResultPromise = session.writeTransaction(async txc => {
-            var result = await txc.run('RETURN "Connection successful!" AS message');
-            return result.records.map(record => record.get('message'));
-        });
-
-        const message = await writeTxResultPromise;
+        await session.run('RETURN "Connection successful!" AS message');
         const timestamp = new Date().toLocaleString();
-        res.send('Connection complete: ' + message + ' at ' + timestamp);
+        res.send('Connection complete at ' + timestamp);
     } catch (error) {
         console.error('Database connection error:', error);
         res.status(500).send('Failed to connect to Neo4j Aura. Error: ' + error.message);
+    } finally {
+        await session.close();
+    }
+});
+
+// Add the endpoint for listing members
+app.get('/list-members', async (req, res) => {
+    const session = driver.session();
+    try {
+        const result = await session.run('MATCH (p:Person) RETURN p');
+        const members = result.records.map(record => record.get('p').properties);
+        res.json(members); // Send response in JSON format
+    } catch (error) {
+        console.error('Error fetching members:', error);
+        res.status(500).json({ error: error.message });
     } finally {
         await session.close();
     }
@@ -48,7 +58,6 @@ app.listen(port, () => {
     console.log("Server running on port", port);
 });
 
-// Close Neo4j driver on server close
 process.on('exit', async () => {
     await driver.close();
 });
