5060870bb6b2e9943d9cc6696da5c0d0f8c47f96
mike0z <mikeoz@grdwrx.com>
2023-12-07 07:56:36 -0600

    update app

--- app.js
+++ app.js
@@ -6,14 +6,15 @@ const neo4j = require('neo4j-driver');
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Neo4j Aura database credentials
 const NEO4J_URI = 'neo4j+s://2320fe1e.databases.neo4j.io';
 const NEO4J_USER = 'neo4j';
 const NEO4J_PASSWORD = 'tktfTZlXVF6lPMwPglT6vl3Hv5NNNcwqsQIq6LpLq-k';
 
+// Create a driver instance, for the user neo4j with password neo4j
 const driver = neo4j.driver(
     NEO4J_URI,
-    neo4j.auth.basic(NEO4J_USER, NEO4J_PASSWORD),
-    { maxTransactionRetryTime: 30000 }
+    neo4j.auth.basic(NEO4J_USER, NEO4J_PASSWORD)
 );
 
 app.use(express.urlencoded({ extended: true }));
@@ -25,18 +26,15 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// Add additional endpoints for other HTML pages if needed
-// Example:
-app.get('/directory', (req, res) => {
-    res.sendFile(path.join(__dirname, 'directory.html'));
-});
-
-// Other routes as defined previously...
+// Add endpoints for other actions like testing connection, listing members, etc.
 
 app.listen(port, () => {
-    console.log("Server running on port", port);
+    console.log(`Server running on port ${port}`);
 });
 
+// Close the Neo4j driver when the Node.js process exits
 process.on('exit', async () => {
     await driver.close();
 });
+
+// Additional functions for handling form submissions, database queries, etc.
