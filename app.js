b83384134fd7434d305e99c14f5a8c078473fcc6
mike0z <mikeoz@grdwrx.com>
2023-12-17 14:54:03 -0600

    fix routes

--- app.js
+++ app.js
@@ -23,13 +23,80 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// ... other existing routes ...
+app.get('/news.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'news.html'));
+});
+
+app.get('/directory.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'directory.html'));
+});
+
+app.get('/home.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'home.html'));
+});
 
 app.get('/react-form', (req, res) => {
     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
 
-// ... other existing routes including /add-person ...
+// Adding route for 'index.html' to address the "Test Add Card Button" issue
+app.get('/index.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Adding route for 'memberlist.html' to return all Person node data
+app.get('/memberlist.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'memberlist.html'));
+});
+
+// Endpoint to test database connection
+app.get('/test-db-connection', async (req, res) => {
+    const session = driver.session();
+    try {
+        await session.run('RETURN "Connection successful!" AS message');
+        const timestamp = new Date().toLocaleString();
+        res.send('Connection complete at ' + timestamp);
+    } catch (error) {
+        console.error('Database connection error:', error);
+        res.status(500).send('Failed to connect to Neo4j Aura. Error: ' + error.message);
+    } finally {
+        await session.close();
+    }
+});
+
+// Endpoint to list members sorted by last name and then first name
+app.get('/list-members', async (req, res) => {
+    const session = driver.session();
+    try {
+        const result = await session.run('MATCH (p:Person) RETURN p ORDER BY p.last_name, p.first_name');
+        const members = result.records.map(record => record.get('p').properties);
+        res.json(members);
+    } catch (error) {
+        console.error('Error fetching members:', error);
+        res.status(500).json({ error: error.message });
+    } finally {
+        await session.close();
+    }
+});
+
+// Maintain the existing endpoint for adding a person
+app.post('/add-person', async (req, res) => {
+    const session = driver.session();
+    try {
+        const { person_id, first_name, middle_name, last_name } = req.body;
+        const result = await session.run(
+            'CREATE (p:Person {person_id: $person_id, first_name: $first_name, middle_name: $middle_name, last_name: $last_name}) RETURN p',
+            { person_id, first_name, middle_name, last_name }
+        );
+        const person = result.records[0].get('p').properties;
+        res.json({ message: 'Person added', person });
+    } catch (error) {
+        console.error('Error adding person:', error);
+        res.status(500).json({ error: error.message });
+    } finally {
+        await session.close();
+    }
+});
 
 app.listen(port, () => {
     console.log("Server running on port", port);
